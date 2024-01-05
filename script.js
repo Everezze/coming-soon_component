a658a07106d497f34bfdd79ef9660a8b2cc9fd15
omarsalim <83906211+Everezze@users.noreply.github.com>
2024-01-05 15:05:04 +0100

    input validation done, button not convering outline fixed

---
+++ script.js
@@ -0,0 +1,30 @@
+const button= document.querySelector("button");
+const emailInput= document.querySelector("input");
+const errorMessage = document.querySelector("form+span");
+const warningIcon = document.querySelector("button .warning-icon");
+//console.log(warningIcon);
+//console.log(emailInput);
+
+const myRegex = /^\w+@\w+\.[a-z]{2,3}$/;
+
+button.addEventListener("click",function(e){
+    console.log(emailInput.value.match(myRegex));
+    if(!emailInput.value.match(myRegex)){
+        e.preventDefault();
+        emailInput.classList.add("error");
+        errorMessage.classList.add("error");
+        button.classList.add("error");
+        warningIcon.classList.add("error");
+    }
+    //console.log("button clicked");
+    //console.log(typeof emailInput.value);
+});
+
+emailInput.addEventListener("input",function(e){
+    if(this.value =="" || this.value.match(myRegex)){
+        emailInput.classList.remove("error");
+        errorMessage.classList.remove("error");
+        button.classList.remove("error");
+        warningIcon.classList.remove("error");
+    }
+});
